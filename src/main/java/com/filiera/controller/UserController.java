300b3fd28493f01ad8136e5b6a7419206129e4b5
lollo <lorenzogio.dimichele@studenti.unicam.it>
2025-06-27 17:27:35 +0200

    Aggiunta springboot alle classi

--- src/main/java/com/filiera/controller/UserController.java
+++ src/main/java/com/filiera/controller/UserController.java
@@ -1,24 +1,46 @@
 package com.filiera.controller;
-
+import com.filiera.model.sellers.Produttore;
 import com.filiera.model.users.User;
 import com.filiera.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
+@RestController
+@RequestMapping("/user")
 public class UserController {
 
     private final UserService service;
+
+    @Autowired
     public UserController(UserService service) { this.service = service; }
 
-
-    public User register(User user) {
+    @PostMapping("/register")
+    public User register(@RequestBody User user) {
         return service.register(user);
     }
 
-    public User getUser(UUID id) {
+    @PostMapping("/register/produttore")
+    public User registerProduttore(@RequestBody Produttore producer) {
+        if (producer == null) {
+            throw new IllegalArgumentException("Produttore cannot be null");
+        }
+        return service.registerProducer(producer);
+    }
+
+    @GetMapping("/all")
+    public Iterable<User> getAllUsers() {
+        return service.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public User getUser(@PathVariable UUID id) {
         return service.findById(id);
     }
 
-    public void deleteById(UUID id) {service.deleteById(id);}
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable UUID id) {service.deleteById(id);}
 
 }
+
--- src/main/java/com/filiera/services/UserServiceImpl.java
+++ src/main/java/com/filiera/services/UserServiceImpl.java
@@ -1,28 +1,61 @@
 package com.filiera.services;
 
 
+import com.filiera.exception.AlreadyInUseEmail;
+import com.filiera.exception.InvalidUserTypeException;
+import com.filiera.model.sellers.Produttore;
 import com.filiera.model.users.User;
+import com.filiera.repository.InMemoryUserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+
+@Service
+@Transactional
 public class    UserServiceImpl implements UserService {
-    private final CrudRepository<User, UUID> repo;
-    public UserServiceImpl(InMemoryUserRepository repo) { this.repo = repo; }
+    private final InMemoryUserRepository userRepo;
+
+    @Autowired
+    public UserServiceImpl(InMemoryUserRepository repo) { this.userRepo = repo; }
     @Override
     public User register(User user) {
 
-        if (repo.findAll().stream().anyMatch(u -> u.getEmail().equals(user.getEmail()))) {
-            throw new IllegalArgumentException("Email già registrata!");
+        if (userRepo.findAll().stream().anyMatch(u -> u.getEmail().equals(user.getEmail()))) {
+            throw new AlreadyInUseEmail("Email already in use!");
         }
-        return repo.save(user);
+        return userRepo.save(user);
+    }
+
+    @Override
+    public Iterable<User> findAll() {
+        return userRepo.findAll();
+    }
+
+
+    @Override
+    public Produttore registerProducer(Produttore producer) {
+        if (producer == null) {
+            throw new IllegalArgumentException("Producer cannot be null");
+        }
+        if (userRepo.findAll().stream().anyMatch(u -> u.getEmail().equals(producer.getEmail()))) {
+            throw new AlreadyInUseEmail("Email already in use!");
+        }
+
+
+        return userRepo.save(producer);
     }
 
     @Override
     public User findById(UUID id) {
-        return repo.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException(("Utente non trovato con id: " + id)));
+        return userRepo.findById(id)
+                .orElseThrow(() -> new InvalidUserTypeException(("User not found with ID : " + id)));
     }
 
-    public void deleteById(UUID id){ repo.deleteById(id); }
+
+
+    public void deleteById(UUID id){ userRepo.deleteById(id); }
 
 }
